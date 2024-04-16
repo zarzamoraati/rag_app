663f32339850ddd70c86af61a097d7171b5abcc8
zarzamoraati <zarzamoraati@gmail.com>
2024-04-16 03:25:04 -0600

    fixing modif

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    "/api":{
-      target:"https://zarzamoraati.github.io/rag_app/",
-      changeOrigin:true,
-      rewrite:(path)=>path.replace(/^\/api/,"")
-    }
-  }
+  base:"https://zarzamoraati.github.io/rag_app/"
 })
