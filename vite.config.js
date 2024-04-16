f5eed2dface880de86f31d1d3da249af39a81556
zarzamoraati <zarzamoraati@gmail.com>
2024-04-16 03:01:54 -0600

    headers

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dns from 'node:dns'
 
 
-dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://zarzamoraati.github.io/rag_app/"
+  base:"https://zarzamoraati.github.io/rag_app/",
+  server:{
+    headers:{
+      allow:"*"
+    }
+  }
 })
